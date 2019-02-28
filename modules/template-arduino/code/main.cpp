684ce560f628c57c34e93ed5d0cdf1ae337a1a24
gerritvanos <gerrit.vanos@student.hu.nl>
2019-02-28 11:50:45 +0100

    added src/header folders an changed main

--- modules/template-arduino/code/main.cpp
+++ modules/template-arduino/code/main.cpp
@@ -2,13 +2,15 @@
 
 int main(void) {
   // kill the watchdog
-  WDT->WDT_MR = WDT_MR_WDDIS;
-  hwlib::wait_ms(1000);
-  hwlib::cout << "this works via arduino";
-
-  if (dksljflskadja) {
-  } else if (condition) {
-  } else if (condition) {
-  }
-  adfkljaldkj
+    WDT->WDT_MR = WDT_MR_WDDIS;
+    hwlib::wait_ms(1000);
+    for (;;){
+        hwlib::cout << "this works via arduino";
+        hwlib::wait_ms(1000);
+    }
 }
+
+
+
+
+
