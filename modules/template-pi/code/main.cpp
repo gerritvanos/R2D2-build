278f2504f8a185d7bb3948abe3a832784fc47066
itzandroidtab <koendegraaff@itzandroidtab.nl>
2019-03-14 23:09:51 +0100

    changed the main to a more hwlib like main

--- modules/template-pi/code/main.cpp
+++ modules/template-pi/code/main.cpp
@@ -1,48 +1,19 @@
-/*
- * blink.c:
- *	Standard "blink" program in wiringPi. Blinks an LED connected
- *	to the first GPIO pin.
- *
- * Copyright (c) 2012-2013 Gordon Henderson. <projects@drogon.net>
- ***********************************************************************
- * This file is part of wiringPi:
- *	https://projects.drogon.net/raspberry-pi/wiringpi/
- *
- *    wiringPi is free software: you can redistribute it and/or modify
- *    it under the terms of the GNU Lesser General Public License as published by
- *    the Free Software Foundation, either version 3 of the License, or
- *    (at your option) any later version.
- *
- *    wiringPi is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public License
- *    along with wiringPi.  If not, see <http://www.gnu.org/licenses/>.
- ***********************************************************************
- */
 
-#include <stdio.h>
-#include <wiringPi.h>
+#include <hwlib.hpp>
 
-// LED Pin - wiringPi pin 0 is BCM_GPIO 17.
+int main(void) {
+    hwlib::cout << "Raspberry Pi blink\n";
 
-#define	LED	0
+    hwlib::target::init();
 
-int main (void)
-{
-  printf ("Raspberry Pi blink\n") ;
+    auto led = hwlib::target::pin_out(hwlib::target::pins::d3);
 
-  wiringPiSetup () ;
-  pinMode (LED, OUTPUT) ;
+    while (true) {
+        led.write(true); // on
+        hwlib::wait_ms(500);
+        led.write(false); // on
+        hwlib::wait_ms(500);
+    }
 
-  for (;;)
-  {
-    digitalWrite (LED, HIGH) ;	// On
-    delay (500) ;		// mS
-    digitalWrite (LED, LOW) ;	// Off
-    delay (500) ;
-  }
-  return 0 ;
+    return 0;
 }
